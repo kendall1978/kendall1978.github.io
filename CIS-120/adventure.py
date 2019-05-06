8e475b75105a5f6cdaa6d9fcd2ed7a08aa575d3f
kendall1978 <46752584+kendall1978@users.noreply.github.com>
2019-05-06 13:43:08 -0500

    adventure update

--- CIS-120/adventure.py
+++ CIS-120/adventure.py
@@ -1,9 +1,21 @@
 #Adventure game 
 #Kendall Roberts
 
-
+import Room 
 class Hero():
-    def heroName():
-        heroName = str(userInput("What is your characters name?: "))
-        return heroName
-    print("Your heros name is: " + heroName())
+    def __init__(Name, Key, Sword, Health):
+        self.Name = input("What is your hero's name? \n")
+        self.Health = 5
+        
+
+
+    def attack():
+        inventory = []
+        damage = 1
+        if x == 2 and y == 2:
+            inventory.append("hasSword")
+            if len(inventory) = 1:
+                damage += 2
+        else:
+            damage = 1
+        return damage
